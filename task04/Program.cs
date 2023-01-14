4334e0925698de24417d3eb63f77ff08ab3f0144
Vitalyi <119963502+applevitamin@users.noreply.github.com>
2023-01-14 04:56:33 +0300

    add task4

---
+++ task04/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int depo = 1;
+while (depo <= number)
+{
+    if(depo%2 == 0)
+    {
+        Console.Write($"{depo} | ");
+    }
+depo++;
+}
