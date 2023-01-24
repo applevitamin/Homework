8d18f098295f5ddeaa8b940d63a498369fe84c9e
Vitalyi <119963502+applevitamin@users.noreply.github.com>
2023-01-25 02:32:24 +0300

    Добавил решение задачи №27

---
+++ hw4_task2(27)/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+void main()
+{
+    Console.WriteLine("Ввод: ");
+    int num = int.Parse(Console.ReadLine()!);
+    System.Console.WriteLine(supernum(num));
+}
+int supernum(int num)
+{
+int sum = 0;
+    for (int i = 0; num > 0; i++)
+    {
+        sum = num % 10 + sum;
+        num = num / 10;
+    }
+    return sum;
+}
+main();
