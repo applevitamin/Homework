54fdee9c072a0b9ca450c9dd33c5b0f355ec34ee
Vitalyi <119963502+applevitamin@users.noreply.github.com>
2023-01-25 01:57:09 +0300

    add sem4_task1(25)

---
+++ hw4_task1(25)/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 25: Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Нельзя использовать библиотеку Math!
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+void main()
+{
+    Console.Clear();
+    System.Console.WriteLine("Введите число А: ");
+    int first = int.Parse(Console.ReadLine()!);
+    System.Console.WriteLine("Введите число В: ");
+    int second = int.Parse(Console.ReadLine()!);
+    Console.WriteLine($"Возвели число {first} в степень числа {second} получили {cucumbers(first, second)}");
+}
+
+int cucumbers(int a, int b)
+{
+    int result = 1;
+
+    for(int i = 0; i < b; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
+
+main();
+
+
+
+
+// Console.WriteLine($"Число {first} и число {Math.Pow(first, second)}");
